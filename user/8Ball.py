a7ce3b81e7fb13ae561f828740eaab386768abb4
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2018-02-21 14:40:51 +1100

    MACKEY, Katie: 2018-02-21 14:40:51

---
+++ user/8Ball.py
@@ -0,0 +1,9 @@
+options = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Do not count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
+q = input("What do you wish to ask the Magic 8-Ball? ")
+import random
+import sys
+while q != "quit":
+    r = random.randint(0,19)
+    print(options[r])
+    q = input("What do you wish to ask the Magic 8-Ball? ")
+sys.exit()
