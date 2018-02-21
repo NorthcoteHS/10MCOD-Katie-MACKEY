f08af1122fc78bcebb1e8906830ba118ef0ff73a
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2018-02-21 14:04:31 +1100

    MACKEY, Katie: 2018-02-21 14:04:31

---
+++ user/Pig Latin.py
@@ -0,0 +1,27 @@
+#eg. Pig Latin is = igPay atinLay isay
+p = input("Translate: ")
+#list version of p
+pl = p.split()
+#list of vowels
+v = ['a','e','i','o','u','A','E','I','O','U']
+#translated list
+t =[]
+#variable for while loop
+a = 0
+#testing first letter
+while a < len(pl):
+    #is it a vowel
+    if pl[a][0] in v:
+        t.append(pl[a] + "ay ")
+    else:
+        #is the second letter also a vowel
+        if pl[a][1] in v:
+            t.append(pl[a][1:]+pl[a][0]+"ay ")
+        #if it's not then do this
+        else:
+            t.append(pl[a][2:]+pl[a][:2]+"ay ")
+    a+= 1
+final = "".join(t)
+print(final)
+
+#Note: this code cannot handle punctuation in a sentence. I don't think I can do that.
