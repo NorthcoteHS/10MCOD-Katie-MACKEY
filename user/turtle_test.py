879203aa3026f0caee91111c3a78ee6a6cf4a293
Katie Mackey <katie.mackey@iinet.net.au>
2018-03-28 14:58:09 +1100

    28.3.18

---
+++ user/turtle_test.py
@@ -0,0 +1,16 @@
+import turtle
+window = turtle.Screen()
+al = turtle.Turtle()
+
+al.left(90)
+al.right(30)
+al.forward(100)
+al.right(120)
+al.forward(100)
+al.penup()
+al.backward(50)
+al.pendown()
+al.right(120)
+al.forward(50)
+
+window.exitonclick()
