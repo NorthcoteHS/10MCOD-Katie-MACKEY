6cd1f8c4b5344caad0d6cbf29a55e42785b69c3b
Katie Mackey <katie.mackey@iinet.net.au>
2018-03-29 12:52:57 +1100

    commented

---
+++ modules/u3_organisationAndReources/naming/circleCalc.py
@@ -0,0 +1,20 @@
+'''
+Prog: circleCalc.py
+Name: Katie Mackey
+Date: 2018-03-29
+Desc: Calculates and returns the area and perimeter of a circle from the radius
+'''
+#welcome mess
+print('Welcome to the Circle Calculator!')
+
+#get radius from user
+r = input('Enter a radius: ')
+#turn into integer
+r = int(r)
+
+#calculate and print area
+area = 3.14 * r * r
+print('The area is', area)
+#calculate and print perimeter
+perimeter = 3.14 * r * 2
+print('The perimeter is', perimeter)
