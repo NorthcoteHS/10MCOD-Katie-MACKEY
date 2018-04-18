af236a1b7252a8f38cb862a49f92a04f50266272
Katie Mackey <katie.mackey@iinet.net.au>
2018-04-18 13:55:54 +1000

    make it a function, edit desc.

--- user/fibonacci.py
+++ user/fibonacci.py
@@ -2,17 +2,17 @@
 prog: fibonacci.py
 Name: Katie
 Date: 2018-04-18
-Desc: generates and prints fibonacci sequence to the Nth number. minimum 2 numbers?
+Desc: generates and prints fibonacci sequence to the Nth number.
 '''
-n = int(input("How many numbers would you like? "))
-if n == 0:
-    print("That's not how numbers work my friend...")
-elif n == 1:
-    print("0, yes 0, the fibonacci sequence starts at 0 not at 1")
-else:
-    num = ["0","1"]
-    i = 2
-    while i < n:
-        num.append(str(int(num[-2])+int(num[-1])))
-        i = i + 1
-    print(", ".join(num))
+def fib(n):
+    if n == 0:
+        print("That's not how numbers work my friend...")
+    elif n == 1:
+        print("0, yes 0, the fibonacci sequence starts at 0 not at 1")
+    else:
+        num = ["0","1"]
+        i = 2
+        while i < n:
+            num.append(str(int(num[-2])+int(num[-1])))
+            i = i + 1
+        print(", ".join(num))
