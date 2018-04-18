f9331360a8eb39af23c319d45f804144861548c3
Katie Mackey <katie.mackey@iinet.net.au>
2018-04-18 13:16:24 +1000

    change to functions

--- user/Caesar 2.0.py
+++ user/Caesar 2.0.py
@@ -31,51 +31,54 @@ for x in string.ascii_uppercase:
 i = 0
 #number value to letter
 b = {v: k for k, v in a.items()}
+#define functions
+def encode():
+    import string
+    i = 0
+    txt = input("Plaintext: ").split()
+    o = inputNumber("Offset: ")
+    ctxt = []
+    for x in txt:
+        while i < len(x):
+            if x[i] in string.ascii_letters:
+                z = a[x[i]]+ o
+                ctxt.append(b.get(z, b.get(z + 26, b.get(z - 26, z))))
+            elif x[i] in string.digits:
+                ctxt.append(str(int(x[i])+ o))
+            else:
+                ctxt.append(x[i])
+            i += 1
+        i = 0
+        ctxt.append(" ")
+    print(''.join(ctxt))
+
+def decode():
+    import string
+    i = 0
+    txt = input("Plaintext: ").split()
+    o = inputNumber("Offset: ")
+    ctxt = []
+    for x in txt:
+        while i < len(x):
+            if x[i] in string.ascii_letters:
+                z = a[x[i]]- o
+                ctxt.append(b.get(z, b.get(z + 26, b.get(z - 26, z))))
+            elif x[i] in string.digits:
+                ctxt.append(str(int(x[i])- o))
+            else:
+                ctxt.append(x[i])
+            i += 1
+        i = 0
+        ctxt.append(" ")
+    print(''.join(ctxt))
 '''
 Here is where the fun began.
 '''
 mode = inputNumber("Encode - 1, Decode - 2 : ")
 while mode:
     if mode == 1:
-   #plaintext
-        txt = input("Plaintext: ").split()
-      #offset for cipher
-        o = inputNumber("Offset: ")
-      #ciphertext
-        ctxt = []
-        for x in txt:
-            while i < len(x):
-                if x[i] in string.ascii_letters:
-                    z = a[x[i]]+ o
-                    ctxt.append(b.get(z, b.get(z + 26, b.get(z - 26, z))))
-                elif x[i] in string.digits:
-                    ctxt.append(str(int(x[i])+o))
-                else:
-                    ctxt.append(x[i])
-                i += 1
-            i = 0
-        ctxt.append(" ")
-        print(''.join(ctxt))
+        encode()
         mode = inputNumber("Encode - 1, Decode - 2 : ")
     elif mode == 2:
-        while mode:
-         #plaintext
-         txt = input("Plaintext: ").split()
-         #offset for cipher
-         o = inputNumber("Offset: ")
-         #ciphertext
-         ctxt = []
-        for x in txt:
-            while i < len(x):
-                if x[i] in string.ascii_letters:
-                    z = a[x[i]]- o
-                    ctxt.append(b.get(z, b.get(z + 26, b.get(z - 26, z))))
-                elif x[i] in string.digits:
-                    ctxt.append(str(int(x[i])-o))
-                else:
-                    ctxt.append(x[i])
-                    i += 1
-            i = 0
-            ctxt.append(" ")
-            print(''.join(ctxt))
-            mode = inputNumber("Encode - 1, Decode - 2 : ")
+        decode()
+        mode = inputNumber("Encode - 1, Decode - 2 : ")
