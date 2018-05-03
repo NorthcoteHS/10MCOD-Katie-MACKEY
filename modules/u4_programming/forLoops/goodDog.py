26ff581a2cb4895ff196eb7da81fec8fd8768f95
Katie Mackey <katie.mackey@iinet.net.au>
2018-05-03 12:10:08 +1000

    goodBoy.py

---
+++ modules/u4_programming/forLoops/goodDog.py
@@ -0,0 +1,9 @@
+'''
+Prog:   goodBoy.py
+Name:   Katie Mackey
+Date:   2018-05-03
+Desc:   Calls the dogs a good boy. A pure program.
+'''
+dogs = ['Spot', 'Rex', 'Bob', 'Rufus']
+for dog in dogs:
+    print("Good boy,", dog+"!")
