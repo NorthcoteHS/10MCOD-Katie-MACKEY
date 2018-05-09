6bda6e1bfcdae7de09099d328eaabec2b082ff21
Katie Mackey <katie.mackey@iinet.net.au>
2018-05-09 15:51:48 +1000

    so it turns out i was very much overcomplicating things...

--- user/subjectMaker.py
+++ user/subjectMaker.py
@@ -4,25 +4,28 @@ Name:   Katie Mackey
 Date:   2018-05-09
 Desc:   add a subject with questions to studyBuddy.py
 '''
-subjects = ["maths","science","history","secret"]
-p = input("would like to add a subject? ")
-if p == "yes":
-    s = input("Please enter the name of the subject: ")
+subjects = ["maths","science","history","history"]
+def create():
+    s = input("Please enter the name of the subject: ").lower()
+    #check if subject already exists
     if s in subjects:
-        print("to finish entering questions hit enter when entered")
-    q = input("Please enter your question: ")
-    while q:
-        a = input("Please enter the answer: ")
-        s[q] = a
-        q = input("Please enter your question: ")
-    else:
-        s = {}
-        subjects.append(s)
-        print("to finish entering questions hit enter when entered")
+        print("To finish entering questions hit enter when entered")
         q = input("Please enter your question: ")
         while q:
             a = input("Please enter the answer: ")
+            s[q]=a
+            q = input("Please enter your question: ")
+    else:
+        print("To finish entering questions hit enter when prompted for your question.")
+        q = input("Please enter your question: ")
+        while q:
+            a = input("Please enter the answer: ")
+            s = dict()
             s[q] = a
             q = input("Please enter your question: ")
+#BEGIN
+p = input("Would you like to add a question? ")
+if p == "yes":
+    create()
 else:
-    break
+    print("ok then.be that way.")
