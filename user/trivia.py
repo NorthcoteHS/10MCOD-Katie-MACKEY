3f66573c2ebc36b7f1b2f80a67e61f5d7566f470
Katie Mackey <katie.mackey@iinet.net.au>
2018-05-09 13:24:03 +1000

    add subject choice and subjects

--- user/trivia.py
+++ user/trivia.py
@@ -15,16 +15,55 @@ maths = {
     "What is another name for the average? ":"mean",
     "The x-intercepts of a graph can be found when y equals what? ":"0",
     "eggs? ":"eggs.",
-    "y=a(x-k)^2+h | what is this form called? ":"turning point form"
+    "y=a(x-k)^2+h | what is this form called? ":"turning point"
 }
-def quiz(subject):
-    for q in subject:
-        a = input(q).lower()
-        if a == subject[q]:
+science = {
+    "The mitochondria is often referred to as the what of the cell?":"powerhouse",
+}
+history = {
+    "x":"x",
+}
+secret = {
+    "x":"x",
+}
+x = input("What subject would you like to revise? ")
+while x:
+    if x == "maths":
+        for q in maths:
+            a = input(q).lower()
+            if maths[q] in a:
+                print("Correct!")
+                score += 1
+            else:
+                print("Incorrect, the answer was", maths[q])
+        print("Your final score was", score,"out of", len(maths), "or", str(score/len(maths)*100)+"%")
+    elif x == "science":
+        for q in science:
+            a = input(q).lower()
+            if science[q] in a:
+                print("Correct!")
+                score += 1
+            else:
+                print("Incorrect, the answer was", science[q])
+        print("Your final score was", score,"out of", len(science), "or", str(score/len(science)*100)+"%")
+    elif x == "history":
+        for q in history:
+            a = input(q).lower()
+            if history[q] in a:
+                print("Correct!")
+                score += 1
+            else:
+                print("Incorrect, the answer was", history[q])
+        print("Your final score was", score,"out of", len(history), "or", str(score/len(history)*100)+"%")
+    elif x == "supersecretsubject":
+        for q in secret:
+            a = input(q).lower()
+        if secret[q] in a:
             print("Correct!")
             score += 1
         else:
-            print("Incorrect, the answer was", subject[q])
-    print("Your final score was", score,"out of", len(subject), "or", str(score/len(subject)*100)+"%")
-x = input("What subject would you like to revise? ")
-quiz(x)
+            print("Incorrect, the answer was", secret[q])
+        print("Your final score was", score,"out of", len(secret), "or", str(score/len(secret)*100)+"%")
+    else:
+        print("Sorry, we don't have any questions for that subject. Please chose from: maths, science or history.")
+    x = input("What subject would you like to revise? ")
