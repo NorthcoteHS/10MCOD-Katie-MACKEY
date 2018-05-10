96644e70f195f101d6c66b88f2abc7045b7f4456
Katie Mackey <katie.mackey@iinet.net.au>
2018-05-10 12:32:26 +1000

    add questions, fix bugs of adding questions and accessing them

--- user/studyBuddy.py
+++ user/studyBuddy.py
@@ -5,8 +5,6 @@ Date:   2018-05-08
 Desc:   FLASHCARDS
 To-do: add more questions
 randomise question order
-add override for typos?
-chose subject (again, more questions)
 
 '''
 score = 0
@@ -15,27 +13,32 @@ maths = {
     "A histogram is used to display what kind of data? ":"numerical",
     "What is another name for the average? ":"mean",
     "The x-intercepts of a graph can be found when y equals what? ":"0",
-    "eggs? ":"eggs.",
     "y=a(x-k)^2+h | what is this form called? ":"turning point"
 }
 science = {
-    "The mitochondria is often referred to as the what of the cell?":"powerhouse",
+    "The mitochondria is often referred to as the what of the cell? ":"powerhouse",
+    "Distance over time is equal to what? ":"velocity",
+    "Covalent bonding occurs between which kind of atoms? ":"non-metal",
+    "Pb is the atomic symbol for which element? ":"lead"
 }
 history = {
     "x":"x",
 }
 secret = {
-    "x":"x",
+    "What is Suga's real name? ":"min yoongi",
+    "How many members are in Seventeen? ":"13",
+    "Who is the main dancer in NCT? ":"ten",
+    "Who is the youngest member of NCT Dream? ":"jisung park"
 }
 def ask(subject,q):
     global score
     a = input(q).lower()
     a = a.strip()
-    if subject[q] in a:
+    if eval(subject)[q] in a:
         print("Correct!")
         score += 1
     else:
-        print("Incorrect, the answer was", subject[q])
+        print("Incorrect, the answer was", eval(subject)[q])
 
 def create():
     global subjects
@@ -54,31 +57,16 @@ def create():
         q = input("Please enter your question: ")+" "
         while q:
             a = input("Please enter the answer: ").lower()
-            s = dict()
-            s[q] = a
+            globals()[s] = dict()
+            globals()[s][q] = a
             q = input("Please enter your question: ")
 
 x = input("What subject would you like to revise? ")
 while x:
-    if x == "maths":
-        for q in maths:
-            ask(maths,q)
-        print("Your final score was", score,"out of", len(maths), "or", str(score/len(maths)*100)+"%")
-        score = 0
-    elif x == "science":
-        for q in science:
-            ask(science,q)
-        print("Your final score was", score,"out of", len(science), "or", str(score/len(science)*100)+"%")
-        score = 0
-    elif x == "history":
-        for q in history:
-            ask(history,q)
-        print("Your final score was", score,"out of", len(history), "or", str(score/len(history)*100)+"%")
-        score = 0
-    elif x == "supersecretsubject":
-        for q in secret:
-            ask(secret,q)
-        print("Your final score was", score,"out of", len(secret), "or", str(score/len(secret)*100)+"%")
+    if x in subjects:
+        for q in eval(x):
+            ask(x,q)
+        print("Your final score was", score,"out of", len(eval(x)), "or", str(score/len(eval(x))*100)+"%")
         score = 0
     else:
         print("Sorry, we don't have any questions for that subject.")
